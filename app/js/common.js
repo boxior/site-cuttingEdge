31c276d279e9e06a3f103f975f1118b5d2b22c2c
JWood <1987boxior@gmail.com>
2017-08-08 19:17:29 +0300

    added adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -14,41 +14,11 @@ $(document).ready(function(){
         $('.content__info2--block div:first-child').css({'top':'0','opacity':'1'});
     });
 
-    // $('.content__info2--block div:first-child').hover(function(){
-    //     $('.content__info2--block div:last-child').animate({'opacity':'1','top':'2rem','right':'2rem'});
-    // }, function(){
-    //     $('.content__info2--block div:last-child').animate({'opacity':'0','top':'0','right':'0'});
-    // });
-
-    $(document).on("mouseover", ".content__green-zone", function(){
-        $('.content__green-zone + .content__shadow').animate({'opacity':'1','top':'2rem','right':'2rem'});
+    $('.content__info2--block div:first-child').hover(function(){
+        $(this.nextElementSibling).animate({'opacity':'1','top':'2rem','right':'2rem'});
+    }, function(){
+        $(this.nextElementSibling).animate({'opacity':'0','top':'0','right':'0'});
     });
-    $(document).on("mouseleave", ".content__green-zone", function(){
-        $('.content__green-zone + .content__shadow').animate({'opacity':'0','top':'0','right':'0'});
-    });
-
-    $(document).on("mouseover", ".content__parking", function(){
-        $('.content__parking + .content__shadow').animate({'opacity':'1','top':'2rem','right':'2rem'});
-    });
-    $(document).on("mouseleave", ".content__parking", function(){
-        $('.content__parking + .content__shadow').animate({'opacity':'0','top':'0','right':'0'});
-    });
-
-    $(document).on("mouseover", ".content__infrastructure", function(){
-        $('.content__infrastructure + .content__shadow').animate({'opacity':'1','top':'2rem','right':'2rem'});
-    });
-    $(document).on("mouseleave", ".content__infrastructure", function(){
-        $('.content__infrastructure + .content__shadow').animate({'opacity':'0','top':'0','right':'0'});
-    });
-
-    $(document).on("mouseover", ".content__four", function(){
-        $('.content__four + .content__shadow').animate({'opacity':'1','top':'2rem','right':'2rem'});
-    });
-    $(document).on("mouseleave", ".content__four", function(){
-        $('.content__four + .content__shadow').animate({'opacity':'0','top':'0','right':'0'});
-    });
-
-
 
     $('.advantages .content__info2').slick({
         infinity: true,
@@ -85,27 +55,13 @@ $(document).ready(function(){
     $('.menu .content__menu').click(function(){
         $('.menu').fadeOut(1000);
     });
-
-    $('.content__nav a:nth-child(2)').click(function(){
-        $('.page1').removeClass('visible');
-        $('.page2').addClass('visible');
-    });
-    
-    $('.content__nav a:nth-child(1)').click(function(){
-        $('.page2').removeClass('visible');
-        $('.page1').addClass('visible');
-    });
     
 
 });
-    // $('.content__nav--page-number').click(function(){
-
-    // });
-
+    
     function animationOut(i){}
     function animationIn(i){}
-      //well, you need modify the cubeTransition.js file
-      //delete the two functions if you dont need this kind of animation.
+    
     
 
 
